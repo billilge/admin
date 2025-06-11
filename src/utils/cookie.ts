c86dd3843c0febd1bbbd368ba1ee43e622824a42
daun-up <awhekdns@kookmin.ac.kr>
2025-06-11 18:56:53 +0900

    remove: 사용하지 않는 쿠키 관련 코드 삭제

--- src/utils/cookie.ts
+++
@@ -1,9 +0,0 @@
-export const getCookie = (name: string): string | null => {
-  const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-  const value = match?.[2];
-  return value?.trim() === '' ? null : (value ?? null);
-};
-
-export function setCookie(name: string, value: string) {
-  document.cookie = `${name}=${encodeURIComponent(value)}; path=/`;
-}
